805ae3843212c1af46aeb2c68beab3cb3c841806
Gabriele Perissinotto <gabrieletorre2008@gmail.com>
2026-02-04 16:12:57 +0100

    Ottimizzato il print dei risultati

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ numnight = int(input("Inserire numero di notti desiderate: "))
 pricexnight = int(input("Inserire il numero per notte: "))
 name = input("Inserite il vostro nome: ")
 totprice = numnight * pricexnight
-print(f"numero di notti: {numnight}")
-print(f"Prezzo per notte: {pricexnight}")
-print(f"Nome: " + name)
-print(f"Prezzo totale: {totprice}€")
+print(f"""numero di notti: {numnight}
+Prezzo per notte: {pricexnight}€
+Nome: {name}
+Prezzo totale: {totprice}€""")
