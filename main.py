ca4dcfb9fdf39bec4636c6777b8d16dec4671b13
Gabriele <gabrieletorre2008@gmail.com>
2026-02-04 14:49:27 +0100

    Funziona adesso

---
+++ main.py
@@ -0,0 +1,8 @@
+numnight = int(input("Inserire numero di notti desiderate: "))
+pricexnight = int(input("Inserire il numero per notte: "))
+name = input("Inserite il vostro nome: ")
+totprice = numnight * pricexnight
+print(f"numero di notti: {numnight}")
+print(f"Prezzo per notte: {pricexnight}")
+print(f"Nome: " + name)
+print(f"Prezzo totale: {totprice}€")
