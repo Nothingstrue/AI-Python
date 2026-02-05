2ad1f0906f6bd3b74ec9608bd2454acc4fa9301d
Gabriele Perissinotto <gabrieletorre2008@gmail.com>
2026-02-05 16:00:20 +0100

    modified main2 and created a first iteration for the Visual Project

--- main2.py
+++ main2.py
@@ -5,4 +5,10 @@ selection = int(0)
 while exitcheck:
     while selection<1 or selection>6 :
         selection = int(input(""">> Benvenuto al resort 'La Sfera'. Inserire l'azione che si vuole svolgere:
-        d"""))
+        >> 1. Inserire utente nel piano vacanze
+        >> 2. Controllare il piano vacanze
+        >> 3. Decidere la vacanza
+        >> 4. Controllare le opzioni disponibili
+        >> 5. Uscire dal programma"""))
+    if selection == 1 :
+        
---
+++ main3.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox
+root = tk.Tk()
+root.title("Try")
+root.geometry("300x200")
+root.mainloop()
